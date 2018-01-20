c54ccc23d99d6a6003aef80d84283f496966550f
Eli <elimaynard923@gmail.com>
2018-01-20 12:57:34 -0500

    Code qual / terminology

--- src/lilt/private/interpret.nim
+++ src/lilt/private/interpret.nim
@@ -36,7 +36,7 @@ Failing is done by raising a RuleError.
 type
     RuleVal* = object of RootObj
         head*: int
-        currentResult*: CurrentResult
+        lambdaState*: LambdaState
 
         case kind*: RuleReturnType:
         of rrtText:
@@ -49,10 +49,10 @@ type
             discard
 
     #[
-    Each new reference / run of a definiton's rule makes a new CurrentResult.
-    This CurrentResult is what the statements in the rule modifies.
+    Each new reference / run of a definiton's rule makes a new LambdaState.
+    This LambdaState is what the statements in the rule modifies.
     ]#
-    CurrentResult* = object of RootObj
+    LambdaState* = object of RootObj
         case kind*: LiltType
         of ltText:
             text*: string
@@ -72,9 +72,9 @@ proc toProperty(rv: RuleVal): inner_ast.Property =
     of rrtNone:
         raise newException(ValueError, "RuleValue but not be of kind rrtNone")
 
-proc hcr(rv: RuleVal): (int, CurrentResult) =
+proc hcr(rv: RuleVal): (int, LambdaState) =
     # No semantic meaning, exists only to make code terser
-    return (rv.head, rv.currentResult)
+    return (rv.head, rv.lambdaState)
 
 proc `$`(s: seq[inner_ast.Node]): string =
     result = "@["
@@ -96,50 +96,50 @@ proc `$`(rv: RuleVal): string =
     of rrtNone:
         return "head: $1, kind: $2" % [$rv.head, $rv.kind]
 
-converter toRuleVal(retVal: (int, string, CurrentResult)): RuleVal =
+converter toRuleVal(retVal: (int, string, LambdaState)): RuleVal =
     return RuleVal(
         kind: rrtText,
         head: retVal[0],
         text: retVal[1],
-        currentResult: retVal[2],
+        lambdaState: retVal[2],
     )
 
-converter toRuleVal(retVal: (int, seq[inner_ast.Node], CurrentResult)): RuleVal =
+converter toRuleVal(retVal: (int, seq[inner_ast.Node], LambdaState)): RuleVal =
     return RuleVal(
         kind: rrtList,
         head: retVal[0],
         list: retVal[1],
-        currentResult: retVal[2],
+        lambdaState: retVal[2],
     )
 
-converter toRuleval(retVal: (int, inner_ast.Node, CurrentResult)): RuleVal =
+converter toRuleval(retVal: (int, inner_ast.Node, LambdaState)): RuleVal =
     return RuleVal(
         kind: rrtNode,
         head: retVal[0],
         node: retVal[1],
-        currentResult: retVal[2],
+        lambdaState: retVal[2],
     )
 
-converter toRuleVal(retVal: (int, CurrentResult)): RuleVal =
+converter toRuleVal(retVal: (int, LambdaState)): RuleVal =
     return RuleVal(
         kind: rrtNone,
         head: retVal[0],
-        currentResult: retVal[1],
+        lambdaState: retVal[1],
     )
 
-proc initCurrentResult*(kind: LiltType): CurrentResult =
+proc initLambdaState*(kind: LiltType): LambdaState =
     case kind:
     of ltText:
-        result = CurrentResult(kind: ltText, text: "")
+        result = LambdaState(kind: ltText, text: "")
     of ltList:
-        result = CurrentResult(kind: ltList, list: @[])
+        result = LambdaState(kind: ltList, list: @[])
     of ltNode:
         # The kind will be added in the top-leve sequence
-        result = CurrentResult(kind: ltNode, node: inner_ast.initNode(""))
+        result = LambdaState(kind: ltNode, node: inner_ast.initNode(""))
     else:
         assert false
 
-type Rule* = proc(head: int, text: string, currentResult: CurrentResult): RuleVal
+type Rule* = proc(head: int, text: string, lambdaState: LambdaState): RuleVal
 type RuleError* = object of Exception
 
 type LiltContext* = TableRef[string, Rule]
@@ -164,10 +164,10 @@ else:
         debugEcho(msg)
 
     proc debugWrap(rule: Rule, node: outer_ast.Node): Rule =
-        proc wrappedRule(head: int, text: string, currentResult: CurrentResult): RuleVal =
+        proc wrappedRule(head: int, text: string, lambdaState: LambdaState): RuleVal =
             debugPush("Attempting to match $1" % $node)
             try:
-                result = rule(head, text, currentResult)
+                result = rule(head, text, lambdaState)
             except RuleError as e:
                 debugPop("Failed: " & e.msg)
                 raise e
@@ -183,17 +183,17 @@ method translate(node: outer_ast.Node, context: LiltContext): Rule {.base.} =
     raise newException(WrongTypeError, "Cannot translate node $1" % $node)
 
 method translate(re: Reference, context: LiltContext): Rule =
-    proc rule(head: int, text: string, currentResult: CurrentResult): RuleVal =
-        # Creating new `currentResult`s is handled in the translate(Lambda) code
-        return context[re.id](head, text, currentResult)
+    proc rule(head: int, text: string, lambdaState: LambdaState): RuleVal =
+        # Creating new `lambdaState`s is handled in the translate(Lambda) code
+        return context[re.id](head, text, lambdaState)
 
     return debugWrap(rule, re)
 
 method translate(lamb: Lambda, context: LiltContext): Rule =
     let innerRule = translate(lamb.body, context)
-    proc rule(head: int, text: string, currentResult: CurrentResult): RuleVal =
+    proc rule(head: int, text: string, lambdaState: LambdaState): RuleVal =
         # Each lambda call gets its own context, so create one.
-        var returnVal = innerRule(head, text, initCurrentResult(lamb.returnType.toLiltType))
+        var returnVal = innerRule(head, text, initLambdaState(lamb.returnType.toLiltType))
 
         if lamb.returnType == rrtNode:
             # TODO either:
@@ -201,54 +201,54 @@ method translate(lamb: Lambda, context: LiltContext): Rule =
             # 2. Disallow returning a node from a lambda
             let isTopLevel = lamb.parent of Definition
             assert isTopLevel
-            returnVal.currentResult.node.kind = lamb.parent.Definition.id
+            returnVal.lambdaState.node.kind = lamb.parent.Definition.id
 
         case lamb.returnType:
         of rrtText:
             # TODO This should not be handled here; types are evidently insufficient
             let hasAdj = lamb.scoped.anyIt(it of Adjoinment)
             if hasAdj:
-                # Text found via mutation of currentResult
-                return (returnVal.head, returnVal.currentResult.text, currentResult)
+                # Text found via mutation of lambdaState
+                return (returnVal.head, returnVal.lambdaState.text, lambdaState)
             else:
                 # Text found via return
-                return (returnVal.head, returnVal.text, currentResult)
+                return (returnVal.head, returnVal.text, lambdaState)
         of rrtNode:
-            return (returnVal.head, returnVal.currentResult.node, currentResult)
+            return (returnVal.head, returnVal.lambdaState.node, lambdaState)
         of rrtList:
-            return (returnVal.head, returnVal.currentResult.list, currentResult)
+            return (returnVal.head, returnVal.lambdaState.list, lambdaState)
         of rrtNone:
             assert false
 
     return debugWrap(rule, lamb)
 
 method translate(se: Sequence, context: LiltContext): Rule =
-    var rule: proc(head: int, text: string, currentResult: CurrentResult): RuleVal
+    var rule: proc(head: int, text: string, lambdaState: LambdaState): RuleVal
 
     case se.returnType:
     of rrtText:
-        rule = proc(head: int, text: string, currentResult: CurrentResult): RuleVal =
+        rule = proc(head: int, text: string, lambdaState: LambdaState): RuleVal =
             var head = head
-            var currentResult = currentResult
+            var lambdaState = lambdaState
             
             var returnText = ""
 
             for node in se.contents:
-                let returnVal = translate(node, context)(head, text, currentResult)
-                (head, currentResult) = returnVal.hcr
+                let returnVal = translate(node, context)(head, text, lambdaState)
+                (head, lambdaState) = returnVal.hcr
                 returnText &= returnVal.text
 
-            return (head, returnText, currentResult)
+            return (head, returnText, lambdaState)
 
     of rrtNone:
-        rule = proc(head: int, text: string, currentResult: CurrentResult): RuleVal =
+        rule = proc(head: int, text: string, lambdaState: LambdaState): RuleVal =
             var head = head
-            var currentResult = currentResult
+            var lambdaState = lambdaState
 
             for node in se.contents:
-                (head, currentResult) = translate(node, context)(head, text, currentResult).hcr
+                (head, lambdaState) = translate(node, context)(head, text, lambdaState).hcr
 
-            return (head, currentResult)
+            return (head, lambdaState)
 
     else:
         assert false
@@ -256,10 +256,10 @@ method translate(se: Sequence, context: LiltContext): Rule =
     return debugWrap(rule, se)
 
 method translate(ch: Choice, context: LiltContext): Rule =
-    proc rule(head: int, text: string, currentResult: CurrentResult): RuleVal =
+    proc rule(head: int, text: string, lambdaState: LambdaState): RuleVal =
         for node in ch.contents:
             try:
-                return translate(node, context)(head, text, currentResult)
+                return translate(node, context)(head, text, lambdaState)
             except RuleError:
                 discard
         raise newException(RuleError, "Didn't match any rule.")
@@ -267,7 +267,7 @@ method translate(ch: Choice, context: LiltContext): Rule =
     return debugWrap(rule, ch)
 
 method translate(li: Literal, context: LiltContext): Rule =
-    proc rule(head: int, text: string, currentResult: CurrentResult): RuleVal =
+    proc rule(head: int, text: string, lambdaState: LambdaState): RuleVal =
         var head = head
         for ci in 0 ..< li.text.len:
             if li.text[ci] == text{head}:
@@ -279,37 +279,37 @@ method translate(li: Literal, context: LiltContext): Rule =
                         $head, $li.text[ci], $text{head}
                     ]
                 )
-        return (head, li.text, currentResult)
+        return (head, li.text, lambdaState)
 
     return debugWrap(rule, li)
 
 method translate(s: Set, context: LiltContext): Rule =
-    proc rule(head: int, text: string, currentResult: CurrentResult): RuleVal =
+    proc rule(head: int, text: string, lambdaState: LambdaState): RuleVal =
         let c = text{head}
         if c in s.charset:
-            return (head + 1, $c, currentResult)
+            return (head + 1, $c, lambdaState)
         raise newException(RuleError, "'$1' not in $2." % [$c, $s])
 
     return debugWrap(rule, s)
 
 method translate(o: Optional, context: LiltContext): Rule =
     let innerRule = translate(o.inner, context)
-    var rule: proc(head: int, text: string, currentResult: CurrentResult): RuleVal
+    var rule: proc(head: int, text: string, lambdaState: LambdaState): RuleVal
 
     if o.inner.returnType in [rrtNone, rrtNode]:
-        rule = proc(head: int, text: string, currentResult: CurrentResult): RuleVal =
-            return innerRule(head, text, currentResult).hcr
+        rule = proc(head: int, text: string, lambdaState: LambdaState): RuleVal =
+            return innerRule(head, text, lambdaState).hcr
 
     else:
-        rule = proc(head: int, text: string, currentResult: CurrentResult): RuleVal =
+        rule = proc(head: int, text: string, lambdaState: LambdaState): RuleVal =
             try:
-                return innerRule(head, text, currentResult)
+                return innerRule(head, text, lambdaState)
             except RuleError:
                 case o.returnType:
                 of rrtList:
-                    return (head, newSeq[inner_ast.Node](), currentResult)
+                    return (head, newSeq[inner_ast.Node](), lambdaState)
                 of rrtText:
-                    return (head, "", currentResult)
+                    return (head, "", lambdaState)
                 else:
                     assert false
 
@@ -318,9 +318,9 @@ method translate(o: Optional, context: LiltContext): Rule =
 method translate(op: OnePlus, context: LiltContext): Rule =
     let innerRule = translate(op.inner, context)
 
-    proc rule(head: int, text: string, currentResult: CurrentResult): RuleVal =
+    proc rule(head: int, text: string, lambdaState: LambdaState): RuleVal =
         var head = head
-        var currentResult = currentResult
+        var lambdaState = lambdaState
 
         var matchedCount = 0
 
@@ -330,11 +330,11 @@ method translate(op: OnePlus, context: LiltContext): Rule =
         while true:
             var retVal: RuleVal
             try:
-                retVal = innerRule(head, text, currentResult)
+                retVal = innerRule(head, text, lambdaState)
             except RuleError:
                 break
 
-            (head, currentResult) = retVal.hcr
+            (head, lambdaState) = retVal.hcr
 
             case op.returnType:
             of rrtText:
@@ -353,11 +353,11 @@ method translate(op: OnePlus, context: LiltContext): Rule =
 
         case op.returnType:
         of rrtList:
-            return (head, returnNodeList, currentResult)
+            return (head, returnNodeList, lambdaState)
         of rrtText:
-            return (head, returnText, currentResult)
+            return (head, returnText, lambdaState)
         of rrtNone:
-            return (head, currentResult)
+            return (head, lambdaState)
         else:
             assert false
 
@@ -366,11 +366,11 @@ method translate(op: OnePlus, context: LiltContext): Rule =
 method translate(g: Guard, context: LiltContext): Rule =
     let innerRule = translate(g.inner, context)
 
-    proc rule(head: int, text: string, currentResult: CurrentResult): RuleVal =
+    proc rule(head: int, text: string, lambdaState: LambdaState): RuleVal =
         try:
-            discard innerRule(head, text, currentResult)
+            discard innerRule(head, text, lambdaState)
         except RuleError:
-            return (head, currentResult)
+            return (head, lambdaState)
         raise newException(RuleError, "Code matched guard inner rule.")
 
     return debugWrap(rule, g)
@@ -378,32 +378,32 @@ method translate(g: Guard, context: LiltContext): Rule =
 method translate(p: outer_ast.Property, context: LiltContext): Rule =
     let innerRule = translate(p.inner, context)
 
-    proc rule(head: int, text: string, currentResult: CurrentResult): RuleVal =
-        var currentResult = currentResult
+    proc rule(head: int, text: string, lambdaState: LambdaState): RuleVal =
+        var lambdaState = lambdaState
         var head = head
 
-        let returnVal = innerRule(head, text, currentResult)
-        (head, currentResult) = returnVal.hcr
-        currentResult.node.properties[p.propName] = returnVal.toProperty
+        let returnVal = innerRule(head, text, lambdaState)
+        (head, lambdaState) = returnVal.hcr
+        lambdaState.node.properties[p.propName] = returnVal.toProperty
 
-        return (head, currentResult)
+        return (head, lambdaState)
     
     return debugWrap(rule, p)
 
 method translate(adj: Adjoinment, context: LiltContext): Rule =
     let innerRule = translate(adj.inner, context)
 
-    proc rule(head: int, text: string, currentResult: CurrentResult): RuleVal =
-        var currentResult = currentResult
-        let returnVal = innerRule(head, text, currentResult)
+    proc rule(head: int, text: string, lambdaState: LambdaState): RuleVal =
+        var lambdaState = lambdaState
+        let returnVal = innerRule(head, text, lambdaState)
 
         case returnVal.kind:
         of rrtText:
-            currentResult.text &= returnVal.text
+            lambdaState.text &= returnVal.text
         else:
             assert false
 
-        return (returnVal.head, currentResult)
+        return (returnVal.head, lambdaState)
 
     return debugWrap(rule, adj)
 
@@ -411,17 +411,17 @@ method translate(adj: Adjoinment, context: LiltContext): Rule =
 method translate(e: Extension, context: LiltContext): Rule =
     let innerRule = translate(e.inner, context)
     
-    proc rule(head: int, text: string, currentResult: CurrentResult): RuleVal =
-        var currentResult = currentResult
-        let returnVal = innerRule(head, text, currentResult)
+    proc rule(head: int, text: string, lambdaState: LambdaState): RuleVal =
+        var lambdaState = lambdaState
+        let returnVal = innerRule(head, text, lambdaState)
 
         case returnVal.kind:
         of rrtNode:
-            currentResult.list.add(returnVal.node)
+            lambdaState.list.add(returnVal.node)
         else:
             assert false
 
-        return (returnVal.head, currentResult)
+        return (returnVal.head, lambdaState)
    
 
     return debugWrap(rule, e)
@@ -444,7 +444,7 @@ proc interpret*(text: string, ruleName: string, input: string): RuleVal =
     let definitions = translate(Program(ast))
     let rule: Rule = definitions[ruleName]
     
-    let ruleVal = rule(0, input, initCurrentResult(
+    let ruleVal = rule(0, input, initLambdaState(
         ast.Program
             .definitions
             .findIt(it.Definition.id == ruleName)
