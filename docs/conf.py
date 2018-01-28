7ed14404e95d4e400d4b44355a5aab28bb837fab
Eli <elimaynard923@gmail.com>
2018-01-28 13:08:55 -0500

    Changed Sphinx theme & fixed .gitignore

--- docs/conf.py
+++ docs/conf.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 #
 # lilt documentation build configuration file, created by
-# sphinx-quickstart on Fri Jan 26 16:52:37 2018.
+# sphinx-quickstart on Sun Jan 28 13:07:35 2018.
 #
 # This file is execfile()d with the current directory set to its
 # containing dir.
@@ -83,7 +83,7 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
