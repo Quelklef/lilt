11853387e5440735628329eaffd52b2bdea39322
Eli <elimaynard923@gmail.com>
2018-01-29 21:29:12 -0500

    Made tests pretter

--- tests/test_parser.nim
+++ tests/test_parser.nim
@@ -6,151 +6,118 @@ import ../src/lilt/private/quick
 import strutils
 
 template test(testName: string, code: string, expected: Node) =
-  let parsed: Node = parseProgram(code)
-  echo "Running test '$1'" % testName
-  if not equiv(parsed, expected):
-    echo "Failed; expected ast:"
-    echo $$expected
-    echo "but got:"
-    echo $$parsed
-    echo "Failed '$1'" % testName
-    assert false
-  echo "Passed"
+    let parsed: Node = parseProgram(code)
+    echo "Running test '$1'" % testName
+    if not equiv(parsed, expected):
+        echo "Failed; expected ast:"
+        echo $$expected
+        echo "but got:"
+        echo $$parsed
+        echo "Failed '$1'" % testName
+        assert false
+    echo "Passed"
 
 
 test(
-  "Parsing Super Duper Easy",
-  "simple: \"simple\"",
-  newProgram(@[
-    "simple" := ^"simple"
-  ])
+    "Parsing Super Duper Easy",
+    "simple: \"simple\"",
+    newProgram(@[
+        "simple" := ^"simple"
+    ])
 )
 
 test(
-  "Parsing 1",
-  """
-  char: *<abcdefg>
-  string: +char
-  """,
-  newProgram(@[
-    newDefinition(
-      "char",
-      newLambda(
-        newOptional(newOnePlus(newSet("abcdefg"))).Node
-      )
-    ).Node,
-    newDefinition(
-      "string",
-      newLambda(
-        newOnePlus(newReference("char")).Node
-      )
-    )
-  ])
+    "Parsing 1",
+    """
+    char: *<abcdefg>
+    string: +char
+    """,
+    newProgram(@[
+        "char" := * <>"abcdefg"
+        , "string" := + @"char"
+    ])
 )
 
 test(
-  "Sequences 1",
-  "ex: \"a\" \"b\"",
-  newProgram(@[
-    "ex" := ~[ ^"a" , ^"b" ]
-  ])
+    "Sequences 1",
+    "ex: \"a\" \"b\"",
+    newProgram(@[
+        "ex" := ~[ ^"a" , ^"b" ]
+    ])
 )
 
 test(
-  "This bug is a damned pain",
-  "ex: &e *[\"b\" &e]",
-  newProgram(@[
-    "ex" := ~[ & @"e", * ~[ ^"b", & @"e" ] ]
-  ])
+    "This bug is a damned pain",
+    "ex: &e *[\"b\" &e]",
+    newProgram(@[
+        "ex" := ~[ & @"e", * ~[ ^"b", & @"e" ] ]
+    ])
 )
 
 test(
-  "Extensions 1",
-  "args: &arg *[\" \" &arg]",
-  newProgram(@[
-    newDefinition(
-      "args",
-      newLambda(newSequence(@[
-        newExtension(newReference("arg")),
-        newOptional(newOnePlus(
-          newSequence(@[
-            newLiteral(" "),
-            newExtension(newReference("arg"))
-          ])
-        ))
-      ]))
-    ).Node
-  ])
+    "Extensions 1",
+    "args: &arg *[\" \" &arg]",
+    newProgram(@[
+        "args" := ~[ & @"arg", * ~[ ^" ", & @"arg" ] ]
+    ])
 )
 
 test(
-  "Adjoinment",
-  """
-  handleString: "'" $*char "'"
-  """,
-  newProgram(@[
-    newDefinition(
-      "handleString",
-      newLambda(newSequence(@[
-        newLiteral("'"),
-        newAdjoinment(newOptional(newOnePlus(newReference("char")))),
-        newLiteral("'")
-      ]))
-    ).Node
-  ])
+    "Adjoinment",
+    """
+    handleString: "'" $*char "'"
+    """,
+    newProgram(@[
+        "handleString" := ~[ ^"'", $: * @"char", ^"'" ]
+    ])
 )
 
 test(
-  "Escape quote",
-  """ex: "\"" """,
-  newProgram(@[
-    "ex" := ^"\\\""
-  ])
+    "Escape quote",
+    """ex: "\"" """,
+    newProgram(@[
+        "ex" := ^"\\\""
+    ])
 )
 
 test(
-  "Escape set",
-  """ex: <\>>""",
-  newProgram(@[
-    "ex" := <>"\\>"
-  ])
+    "Escape set",
+    """ex: <\>>""",
+    newProgram(@[
+        "ex" := <>"\\>"
+    ])
 )
 
 test(
-  "Escapes most",
-  """ex: "\t\r\c\l\a\b\e\\" """,
-  newProgram(@[
-    "ex" := ^"\\t\\r\\c\\l\\a\\b\\e\\\\"
-  ])
+    "Escapes most",
+    """ex: "\t\r\c\l\a\b\e\\" """,
+    newProgram(@[
+        "ex" := ^"\\t\\r\\c\\l\\a\\b\\e\\\\"
+    ])
 )
 
 #[ test(
-  "Comments 1",
-  """
-   /( Block comment! )
-   / Line comment
-  vowel: <aeiou>/This is a comment
-  /(/( nest a comment
-  Let's add /() some code in the comments:
-  code: <code> ))
-  """,
-  newProgram(@[
-    newDefinition(
-      "vowel",
-      newLambda(newSet("aeiou"))
-    ).Node
-  ])
+    "Comments 1",
+    """
+     /( Block comment! )
+     / Line comment
+    vowel: <aeiou>/This is a comment
+    /(/( nest a comment
+    Let's add /() some code in the comments:
+    code: <code> ))
+    """,
+    newProgram(@[
+        newDefinition(
+            "vowel",
+            newLambda(newSet("aeiou"))
+        ).Node
+    ])
 ) ]#
 
 test(
-  "Lambda 1",
-  "lambTest: { <a> }",
-  newProgram(@[
-    newDefinition(
-      "lambTest",
-      newLambda(
-        newLambda(newSet("a"))
-      )
-    ).Node
-  ])
+    "Lambda 1",
+    "lambTest: { <a> }",
+    newProgram(@[
+        "lambTest" := % <>"a"
+    ])
 )
