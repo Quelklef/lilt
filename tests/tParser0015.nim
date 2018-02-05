9e8e09df5b85b68f6acfc4ffca8984708fe940db
Eli <elimaynard923@gmail.com>
2018-02-04 22:49:31 -0500

    Added a test & fixed version

---
+++ tests/tParser0015.nim
@@ -0,0 +1,18 @@
+
+include hparser
+
+const spec = slurp("ParserSpec0015.lilt")
+test(
+  spec,
+  newProgram(@[
+      "__" := ~[ ! @"newline", @"_" ]
+    , "identifier" := ~[ @"alpha", * @"alphanum" ]
+    , "typedArg" %= ~[ "name" .= @"identifier", @"__", ^":", @"__", "type" .= @"identifier" ]
+    , "lambda" %= ~[ "args" .= % + ~[ & @"typedArg", @"__" ], ^":", "body" .= @"expression" ]
+    , "expression" := |[ @"lambda", @"literal" ]
+    , "literal" := |[ @"charLiteral", @"strLiteral", @"numberLiteral" ]
+    , "charLiteral" %= ~[ ^"'", "text" .= @"any" ]
+    , "strLiteral" %= ~[ ^"\"", "text" .= * @"any", ^"\"" ]
+    , "numberLiteral" %= ( "text" .= % * ~[ $: @"digit", ? ^"_" ] )
+  ])
+)
